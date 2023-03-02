c705df3744c1e3fe863752fadf21840c5516dd75
Alok Bhattarai <87488913+banalok@users.noreply.github.com>
2023-03-02 16:30:28 -0500

    Update intensity_app_1.py

--- intensity_app_1.py
+++ intensity_app_1.py
@@ -118,15 +118,15 @@ def show_video(img):
         img_v = Image.fromarray(img_arr)
         video_frames.append(img_v)
     height, width = video_frames[0].size
-    command = 'ffmpeg -framerate 30 -i frame_%d.tif -c:v libx264 -preset slow -crf 22 output.mp4'
+    command = 'ffmpeg -framerate 30 -i frame_%d.tif -c:v libx264 -preset slow -crf 22 /app/output.mp4'
     video_file = subprocess.call(command, shell=True)
     # fourcc = cv2.VideoWriter_fourcc(*'H264')
     # video = cv2.VideoWriter('output.mp4', fourcc, 10, (width,height))
     # for im in video_frames:
     #     video.write(np.array(im))
-    video_bytes = BytesIO(video_file.read())
-    #video_file_1 = open(video_bytes, 'rb')
-    return video_bytes
+    video_file = open('output.mp4', 'rb')
+    #video_bytes = BytesIO(video_file.read())
+    return  video_file
     
 def main():
     # selected_box = st.sidebar.selectbox(
