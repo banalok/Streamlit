968bde88c1268fc7c4c3fe555f903c78c170639b
Alok Bhattarai <87488913+banalok@users.noreply.github.com>
2023-04-12 22:29:11 -0400

    Update 🔬_Preprocessing_and_Segmentation.py

--- 🔬_Preprocessing_and_Segmentation.py
+++ 🔬_Preprocessing_and_Segmentation.py
@@ -166,7 +166,7 @@ def Segment():
     st.title('**_Segmentation of a tiff stack_**')
     #if "raw_file" not in st.session_state:
     raw_file = st.file_uploader("*_Choose an image file_*") 
-    st.image(raw_file)
+    #st.image(raw_file)
     st.write(raw_file)               
     #else:
         #st.write(st.session_state.raw_file)
