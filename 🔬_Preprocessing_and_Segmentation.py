1d4cdba0b70ef73279547b4962a611a8e41f5135
Alok Bhattarai <87488913+banalok@users.noreply.github.com>
2023-04-24 18:36:20 -0400

    Update 🔬_Preprocessing_and_Segmentation.py

--- 🔬_Preprocessing_and_Segmentation.py
+++ 🔬_Preprocessing_and_Segmentation.py
@@ -170,11 +170,11 @@ def Segment():
     
     if st.session_state.raw_file is not None:
         st.warning('Please reload the page to upload a new file') 
-        st.image(st.session_state.raw_file,use_column_width=True,clamp = True)
+        #st.image(st.session_state.raw_file,use_column_width=True,clamp = True)
     else:
         st.session_state.raw_file = st.file_uploader("*_Choose an image file_*")
         #st.image(st.session_state.raw_file,use_column_width=True,clamp = True)
-    st.write(st.session_state.raw_file)
+    
     if st.session_state.raw_file is not None:
         #plt.save(raw_file, cwd)
         ######use this script to load the image on the deployed app############
