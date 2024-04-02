571ac49c9bb45dcadfdf99f91d39a26364b63f7f
Alok Bhattarai <alokkbh26@gmail.com>
2024-04-02 13:31:01 -0400

    Read_txt

---
+++ pages/03_📝_Read_Me.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def load_readme():
+    with open("README.md", "r", encoding="utf-8") as file:
+        readme_text = file.read()
+    return readme_text
+
+readme_text = load_readme()
+st.markdown(readme_text)
